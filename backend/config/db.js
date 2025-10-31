2ab87abcfeec449cc50a90e55c07262fc0542372
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-31 20:37:58 +0530

    Connected to DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 const connectDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("connect to DB");
+        console.log("connected to DB");
     }catch(err){
         console.log(err.message);
         process.exit(1);
