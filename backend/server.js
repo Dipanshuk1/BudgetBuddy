1081bb96bc4737b88a029664ff520b75ec159215
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-11-01 22:07:02 +0530

    imported transaction routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
 
 dotenv.config();
 const app=express();
@@ -8,6 +11,14 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Budget Buddy API is running 🚀");
+});
 
 const PORT=process.env.PORT||4000;
 app.listen(PORT, () => {
